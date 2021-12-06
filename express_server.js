ec088808fbe30956a14b9afa77779d4fab600bd5
Erica-jihyeon <jihyeon20@gmail.com>
2021-12-06 23:57:52 +0000

    add a second route with route param

--- express_server.js
+++ express_server.js
@@ -1,3 +1,4 @@
+const { request } = require('express');
 const express = require("express");
 const app = express();
 const PORT = 8080;
@@ -26,6 +27,11 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 })
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[this.shortURL] }
+  res.render("urls_show", templateVars);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
