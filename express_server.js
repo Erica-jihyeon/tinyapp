76ab5921881e6cf39490a16d07e0c618b820de90
Erica-jihyeon <jihyeon20@gmail.com>
2021-12-09 20:54:43 +0000

    minor correction before starting cookie session

--- express_server.js
+++ express_server.js
@@ -227,8 +227,8 @@ function generateRandomString() {
 
 function getUserByEmail(email, database) {
   
-  for (const user in users) {
-    if (users[user].email === email) return users[user];
+  for (const user in database) {
+    if (database[user].email === email) return database[user];
   }
   return false;
 };
